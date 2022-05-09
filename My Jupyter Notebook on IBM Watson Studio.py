83f77ec8f617e87317c5409716d4307768f43ee3
crhdez <35966298+crhdez@users.noreply.github.com>
2022-05-09 12:23:06 -0400

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Cinthya Rodriguez** 
+# 
+# Data Analyst
+
+# _I am interested in data science because I want to better undestand of data impacts our life and our choices_.
+
+# ### The code is supposed to print "Hello World"
+
+# In[8]:
+
+
+print("Hello World")
+
+
+# <dl>
+#   <dt>Definition list</dt>
+#     <dd>1. Item 1</dd>
+# <dd> 2. Item 2</dd>
+# <dd> * Item 2.1</dd> 
+# <dd> * Item 2.2</dd> 
+#     </dl>
+# [Link to google](https://www.google.com)
+# 
+
+# In[ ]:
+
+
+
+
